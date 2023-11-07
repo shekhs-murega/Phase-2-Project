4ba0f04abbd92db2cf18c085c3533a7f5661f797
shekinah <nyamburamary1212@gmail.com>
2023-11-07 22:29:32 +0300

    About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <h1>About My App</h1>
-      <p>This is the about page.</p>
+      <p> This is a web application that allows you to view information about various dog breeds.
+        It's features include:
+          viewing a list of dog breeds,
+           selecting a breed to view its details and
+          displaying breed information, including description, life expectancy, and weight.</p>
+      <div>
+        <img 
+          src="https://media.giphy.com/media/U5dPt0qZ7F1ks/giphy.gif" 
+          alt="dog giphy" 
+          style={{ width: '600px', height: '600px' }}
+        />
+      </div>
     </div>
   );
 }
