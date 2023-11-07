10e5973e00d0019c9a82b2abddd13b77b0ab789a
shekinah <nyamburamary1212@gmail.com>
2023-11-07 22:39:13 +0300

    About component

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,9 @@ function About() {
       <h1>About My App</h1>
       <p> This is a web application that allows you to view information about various dog breeds.
         It's features include:
-          viewing a list of dog breeds,
+           viewing a list of dog breeds,
            selecting a breed to view its details and
-          displaying breed information, including description, life expectancy, and weight.</p>
+           displaying breed information, including description, life expectancy, and weight.</p>
       <div>
         <img 
           src="https://media.giphy.com/media/U5dPt0qZ7F1ks/giphy.gif" 
