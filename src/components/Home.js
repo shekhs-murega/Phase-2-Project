7761278c01affc78724d9e51e010175bbc48b9ec
shekinah <nyamburamary1212@gmail.com>
2023-11-07 22:09:06 +0300

    Populating Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <h1>Welcome to My App!</h1>
-      <p>This is the home page.</p>
+      <a href="https://www.example.com/about">
+        <img 
+          src="https://media.giphy.com/media/f3qJsJ6C3F1X51OwsQ/giphy.gif" 
+          alt="Dog giphy" 
+          style={{ width: '900px', height: '900px' }}
+        />
+      </a>
     </div>
   );
 }
