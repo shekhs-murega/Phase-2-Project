375d969ed4edeb8c1b1b521a92af30b45f4651a4
shekinah <nyamburamary1212@gmail.com>
2023-11-08 13:54:38 +0300

    About component

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ function About() {
         className="about-image"
       />
       <div className="about-description">
-        <h1>About My App</h1>
+        <h1>Dog Breed App</h1>
         <p>
         "The Dog Breed App is a user-friendly web application designed for dog enthusiasts and curious minds alike. With this app, you can explore a vast array of dog breeds, each with its unique characteristics and charm. Browse through a comprehensive list of breeds, select one that piques your interest, and delve into detailed descriptions, including life expectancy and weight ranges. It's your one-stop source for canine knowledge, helping you make informed decisions about your future furry companions. Whether you're a seasoned dog lover or just beginning your journey into the world of canines, the Dog Breed App is your trusted companion for all things dog-related."
         </p>
