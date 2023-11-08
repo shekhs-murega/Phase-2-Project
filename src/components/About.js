a9e8622eef2e79e52d9153774c24e0f58a0dbd92
shekinah <nyamburamary1212@gmail.com>
2023-11-08 13:20:07 +0300

    Styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import './About.css'; // Import your CSS file
 
 function About() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>About My App</h1>
-      <p> This is a web application that allows you to view information about various dog breeds.
-        It's features include:
-           viewing a list of dog breeds,
-           selecting a breed to view its details and
-           displaying breed information, including description, life expectancy, and weight.</p>
-      <div>
-        <img 
-          src="https://media.giphy.com/media/U5dPt0qZ7F1ks/giphy.gif" 
-          alt="dog giphy" 
-          style={{ width: '600px', height: '600px' }}
-        />
+    <div className="about-container">
+      <img
+        src="https://media.giphy.com/media/U5dPt0qZ7F1ks/giphy.gif"
+        alt="dog giphy"
+        className="about-image"
+      />
+      <div className="about-description">
+        <h1>About My App</h1>
+        <p>
+          This is a web application that allows you to view information about various dog breeds.
+          Its features include:
+          viewing a list of dog breeds,
+          selecting a breed to view its details, and
+          displaying breed information, including description, life expectancy, and weight.
+        </p>
       </div>
     </div>
   );
