23840b44bb89ffbdf343fcadb6b1c91ed9e04b13
shekinah <nyamburamary1212@gmail.com>
2023-11-08 22:40:51 +0300

    Data component

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,51 +3,59 @@ import React, { useState, useEffect } from 'react';
 function Data() {
   const [breeds, setBreeds] = useState([]);
   const [selectedBreed, setSelectedBreed] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Fetch dog breeds from the API and update the state
     fetch('https://dogapi.dog/api/v2/breeds?page[number]=1')
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setBreeds(data.data);
+        setLoading(false); // Mark loading as false when data is loaded
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
+        setLoading(false); // Mark loading as false in case of an error
       });
   }, []);
 
-  const handleBreedClick = breedName => {
-    // Set the selected breed when a breed is clicked
-    setSelectedBreed(breeds.find(breed => breed.attributes.name === breedName));
+  const handleBreedClick = (breedName) => {
+    setSelectedBreed(breeds.find((breed) => breed.attributes.name === breedName));
   };
 
   return (
     <div>
       <h1>Dog Breeds:</h1>
-      <ul>
-        {breeds.map(breed => (
-          <li
-            key={breed.id}
-            onClick={() => handleBreedClick(breed.attributes.name)}
-          >
-            {breed.attributes.name}
-          </li>
-        ))}
-      </ul>
-      {selectedBreed && (
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
         <div>
-          <h2>Selected Breed: {selectedBreed.attributes.name}</h2>
-          <p>Description: {selectedBreed.attributes.description}</p>
-          <p>Maximum Age: {selectedBreed.attributes.life.max} years</p>
-          <p>Minimum Age: {selectedBreed.attributes.life.min} years</p>
-          <p>
-            Male Weight: {selectedBreed.attributes.male_weight.min} -{' '}
-            {selectedBreed.attributes.male_weight.max} kg
-          </p>
-          <p>
-            Female Weight: {selectedBreed.attributes.female_weight.min} -{' '}
-            {selectedBreed.attributes.female_weight.max} kg
-          </p>
+          <ul>
+            {breeds.map((breed) => (
+              <li
+                key={breed.id}
+                onClick={() => handleBreedClick(breed.attributes.name)}
+              >
+                {breed.attributes.name}
+              </li>
+            ))}
+          </ul>
+          {selectedBreed && (
+            <div>
+              <h2>Selected Breed: {selectedBreed.attributes.name}</h2>
+              <p>Description: {selectedBreed.attributes.description}</p>
+              <p>Maximum Age: {selectedBreed.attributes.life.max} years</p>
+              <p>Minimum Age: {selectedBreed.attributes.life.min} years</p>
+              <p>
+                Male Weight: {selectedBreed.attributes.male_weight.min} -{' '}
+                {selectedBreed.attributes.male_weight.max} kg
+              </p>
+              <p>
+                Female Weight: {selectedBreed.attributes.female_weight.min} -{' '}
+                {selectedBreed.attributes.female_weight.max} kg
+              </p>
+            </div>
+          )}
         </div>
       )}
     </div>
