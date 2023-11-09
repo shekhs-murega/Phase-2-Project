07429a442b7e0b34bc03b405a1fbc22d5c290984
shekinah <nyamburamary1212@gmail.com>
2023-11-09 10:26:59 +0300

    Adding sticker and styling

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './Data.css';
 
 function Data() {
   const [breeds, setBreeds] = useState([]);
@@ -24,12 +25,12 @@ function Data() {
   };
 
   return (
-    <div>
-      <h1>Dog Breeds:</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
+    <div className="data-container">
+      <div className="list-details">
+        <h1>Dog Breeds:</h1>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
           <ul>
             {breeds.map((breed) => (
               <li
@@ -40,24 +41,31 @@ function Data() {
               </li>
             ))}
           </ul>
-          {selectedBreed && (
-            <div>
-              <h2>Selected Breed: {selectedBreed.attributes.name}</h2>
-              <p>Description: {selectedBreed.attributes.description}</p>
-              <p>Maximum Age: {selectedBreed.attributes.life.max} years</p>
-              <p>Minimum Age: {selectedBreed.attributes.life.min} years</p>
-              <p>
-                Male Weight: {selectedBreed.attributes.male_weight.min} -{' '}
-                {selectedBreed.attributes.male_weight.max} kg
-              </p>
-              <p>
-                Female Weight: {selectedBreed.attributes.female_weight.min} -{' '}
-                {selectedBreed.attributes.female_weight.max} kg
-              </p>
-            </div>
-          )}
-        </div>
-      )}
+        )}
+        {selectedBreed && (
+          <div>
+            <h2>Selected Breed: {selectedBreed.attributes.name}</h2>
+            <p>Description: {selectedBreed.attributes.description}</p>
+            <p>Maximum Age: {selectedBreed.attributes.life.max} years</p>
+            <p>Minimum Age: {selectedBreed.attributes.life.min} years</p>
+            <p>
+              Male Weight: {selectedBreed.attributes.male_weight.min} -{' '}
+              {selectedBreed.attributes.male_weight.max} kg
+            </p>
+            <p>
+              Female Weight: {selectedBreed.attributes.female_weight.min} -{' '}
+              {selectedBreed.attributes.female_weight.max} kg
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="image-container">
+        <img
+          src="https://media.giphy.com/media/3oFzlW8dht4DdvwBqg/giphy.gif"
+          alt="Dog sticker"
+          className="breed-image"
+        />
+      </div>
     </div>
   );
 }
